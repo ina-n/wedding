7f1ce0efa56ae68ab24ba15effd3d26431cbac86
ina-n <inaeneugebauer@gmail.com>
2016-08-16 11:15:58 -0400

    bug fixes

--- js/main.js
+++ js/main.js
@@ -4,7 +4,33 @@ $(document).ready(function(){
 		window.open($(this).attr('href'), 'popupWindow', 'width=600, height=600');
 
 	});
-		 
+		
+
+	$('a.gallery[rel="gal1"]').colorbox({
+				'opacity': 0.95,
+				 'rel': 'gal1',
+				 
+			});
+			$('a.gallery[rel="gal2"]').colorbox({
+				'opacity': 0.95,
+				 'rel': 'gal2',
+				 
+			});
+
+			$('a.gallery[rel="gal3"]').colorbox({
+				'opacity': 0.95,
+				 'rel': 'gal3',
+				 
+			});
+
+				$('a.gallery[rel="gal4"]').colorbox({
+				'opacity': 0.95,
+				 'rel': 'gal4',
+				 
+			});	 
 	
 
+	
 });
+
+
